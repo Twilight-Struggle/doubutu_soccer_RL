99d96cd1e2a8cb00916644e1c4392e7b6b3a615d
Twilight-Struggle <pikupiken@yahoo.co.jp>
2020-06-21 19:35:49 +0900

    キーのリストのバイトカウント実装

--- playerRemix.py
+++ playerRemix.py
@@ -6,6 +6,41 @@ from includes import PlayPos
 from includes import list_to_tuple
 import includes
 
+import sys
+from itertools import chain
+from collections import deque
+
+
+def compute_object_size(o, handlers={}):
+    dict_handler = lambda d: chain.from_iterable(d.items())
+    all_handlers = {
+        tuple: iter,
+        list: iter,
+        deque: iter,
+        dict: dict_handler,
+        set: iter,
+        frozenset: iter,
+    }
+    all_handlers.update(handlers)  # user handlers take precedence
+    seen = set()  # track which object id's have already been seen
+    default_size = sys.getsizeof(
+        0)  # estimate sizeof object without __sizeof__
+
+    def sizeof(o):
+        if id(o) in seen:  # do not double count the same object
+            return 0
+        seen.add(id(o))
+        s = sys.getsizeof(o, default_size)
+
+        for typ, handler in all_handlers.items():
+            if isinstance(o, typ):
+                s += sum(map(sizeof, handler(o)))
+                break
+        return s
+
+    return sizeof(o)
+
+
 BOARD_WIDTH = 3
 BOARD_HEIGHT = 5
 
@@ -266,6 +301,9 @@ class Qlearning(Player):
             includes.fuckinglobal += 1
         else:
             includes.fuckinglobal2 += 1
+        print("Used key byte:{}".format(
+            compute_object_size((list_to_tuple(boardcells), Act.move_command,
+                                 Act.kick_command))))
         return gotQvalue
 
     def getGameResult(self, Board, winner):
